dd74d098939805dc20b30f94fac1c9b5309bc1e2
Sunil Sahani <107791326+Sahani26@users.noreply.github.com>
2024-10-18 11:00:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
  import Header from "./components/Header";
  import Energy from "./components/Energy";
  import Footer from './components/Footer';
- import Service from './components/Ourservices';
+ import Service from './components/OurServices';
  
  
  
